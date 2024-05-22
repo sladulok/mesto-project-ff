7659ef71a29f38b1627044c2fc9b195ec32864db
sladulok <sladulok@yandex.com>
2024-05-22 19:35:21 +0300

    add alt

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,7 @@ function createCard(cardData, onDeleteCard) {
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
     cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
     cardElement.querySelector('.card__title').textContent = cardData.name;
  
     deleteButton.addEventListener('click', () => {
